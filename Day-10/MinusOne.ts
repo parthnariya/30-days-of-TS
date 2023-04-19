2a4b771bb9dfc3ff9f700d7c3c41d3efb6c49c94
parthnariya <parthknariya1919@gmail.com>
2023-04-19 11:40:02 +0530

    day-10 challenge-2

---
+++ Day-10/MinusOne.ts
@@ -0,0 +1,16 @@
+//Given a number (always positive) as a type.type should return the number decreased by one.
+// (Given a number (always positive) as a type)
+/* type Zero = MinusOne<1> // 0
+type FiftyFour = MinusOne<55> // 54 */
+
+/* 1. we can define some temporary tuple with length of given number and return expected number */
+
+type MinusOne<N extends number, Temp extends any[] = []> = [
+  ...Temp,
+  1
+]["length"] extends N
+  ? Temp["length"]
+  : MinusOne<N, [...Temp, 1]>;
+
+type Zero = MinusOne<1>; // 0
+type FiftyFour = MinusOne<55>; // 54
